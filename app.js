67248e6d5586a1533ee83171d2c00c0619ff3c9c
Ritwick Dey <ritwickdey@outlook.com>
2017-12-17 13:26:50 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var appRoutes = require('./routes/app');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/', appRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.render('index');
+});
+
+module.exports = app;
