045b5f81fb01446437f91d1ea518e657154306c5
Ritwick Dey <ritwickdey@outlook.com>
2017-12-17 16:45:14 +0530

    aot setup

---
+++ assets/app/main.aot.ts
@@ -0,0 +1,10 @@
+import './polyfills';
+
+import { platformBrowser } from '@angular/platform-browser'
+import { AppModule } from './app.module';
+import { enableProdMode } from '@angular/core';
+
+import { AppModuleNgFactory } from './app.module.ngfactory';
+
+enableProdMode();
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
