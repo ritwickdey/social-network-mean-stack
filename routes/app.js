83e05961663f8f2de799208ca2c2d2873197df54
Ritwick Dey <ritwickdey@outlook.com>
2017-12-19 17:38:40 +0530

    cleanup for SPA

--- routes/app.js
+++ routes/app.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('node');
+    res.render('index');
 });
 
-
-router.post('/', function (req, res, next) {
-    const email = req.body.email;
-    const user = new User({
-        firstName: 'Akash',
-        lastName: 'Dey',
-        password: 'pass',
-        email: email
-    });
-
-    user.save()
-        .then(e => {
-            res.redirect('/');
-        });
-});
 
 module.exports = router;
