e6651ba59be9cb3e799f0b3591c771c52eb59775
Ritwick Dey <ritwickdey@outlook.com>
2017-12-23 21:53:12 +0530

    isLoggedin()

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -27,4 +27,8 @@ export class AuthService {
         localStorage.clear();
     }
 
+    isLoggedIn() {
+        return localStorage.getItem('token') !== null;
+    }
+
 }
--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AuthenticationComponent implements OnInit {
-    constructor() { }
+    constructor(private authService: AuthService) { }
 
     ngOnInit() { }
+
+    isLoggedIn() {
+        return this.authService.isLoggedIn();
+    }
 }
