ddfffa4ac355ba47f2da72f95a37e10a40d2cdcf
Ritwick Dey <ritwickdey@outlook.com>
2017-12-23 18:24:42 +0530

    POST: /USER

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-router.get('/', function (req, res, next) {
-    res.render('index');
+const User = require('../models/user');
+
+router.post('/', (req, res, next) => {
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+    });
+
+    user.save().then(result => {
+            res.status(201).json({
+                message: 'User Created',
+                obj: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                title: 'An Error Occurred',
+                error: err
+            });
+        });
 });
 
 
