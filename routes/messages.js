7cd6f475cd7de851378db57788ac10d708a6a4cf
Ritwick Dey <ritwickdey@outlook.com>
2017-12-24 01:03:24 +0530

    Only the user who've created msg can delete or edit msg :smile:

--- routes/messages.js
+++ routes/messages.js
@@ -68,6 +68,7 @@ router.post('/', (req, res, next) => {
 });
 
 router.patch('/:id', (req, res, next) => {
+    const decoded = jwt.decode(req.query.token);
     Message.findById(req.params.id)
         .then(message => {
             if (!message)
@@ -77,15 +78,22 @@ router.patch('/:id', (req, res, next) => {
                         message: 'Message Not Found!'
                     }
                 });
+            if (message.user != decoded.user._id) {
+                return res.status(401).json({
+                    title: 'Not Authenticated',
+                    error: {
+                        message: `Get Out! You don't have permission!`
+                    }
+                });
+            }
 
             message.content = req.body.content;
-            message.save()
-                .then(result => {
-                    res.status(200).json({
-                        message: 'Updated Message',
-                        obj: result
-                    });
+            message.save().then(result => {
+                res.status(200).json({
+                    message: 'Updated Message',
+                    obj: result
                 });
+            });
         })
         .catch(err => {
             console.log(err);
@@ -98,7 +106,8 @@ router.patch('/:id', (req, res, next) => {
 });
 
 router.delete('/:id', (req, res, next) => {
-    Message.findByIdAndRemove(req.params.id)
+    const decoded = jwt.decode(req.query.token);
+    Message.findById(req.params.id)
         .then(message => {
             if (!message) {
                 return res.status(404).json({
@@ -108,9 +117,21 @@ router.delete('/:id', (req, res, next) => {
                     }
                 });
             }
-            res.status(200).json({
-                message: 'Message Deleted',
-                obj: message
+
+            if (message.user != decoded.user._id) {
+                return res.status(401).json({
+                    title: 'Not Authenticated',
+                    error: {
+                        message: `Get Out! You don't have permission!`
+                    }
+                });
+            }
+
+            message.remove().then(() => {
+                res.status(200).json({
+                    message: 'Message Deleted',
+                    obj: message
+                });
             });
 
         })
