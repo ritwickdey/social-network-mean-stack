4e7571443ff2b63acc2c72c3c23f46d417932699
Ritwick Dey <ritwickdey@outlook.com>
2017-12-23 13:28:08 +0530

    GET /MESSAGE

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,23 @@ const router = express.Router();
 
 const Message = require('../models/message');
 
+router.get('/', (req, res, next) => {
+    Message.find()
+        .then(messages => {
+            res.status(200).json({
+                message: 'Suceess',
+                obj: messages
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                title: 'An Error Occurred',
+                error: err
+            });
+        });
+});
+
 router.post('/', (req, res, next) => {
     const message = new Message({
         content: req.body.content
