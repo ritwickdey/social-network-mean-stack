13a3149bc4f0179c0069cb9e82ddfa8bdbbfc128
Ritwick Dey <ritwickdey@outlook.com>
2017-12-23 16:45:49 +0530

    PATCH : /Message/:id

--- routes/messages.js
+++ routes/messages.js
@@ -41,5 +41,34 @@ router.post('/', (req, res, next) => {
         });
 });
 
+router.patch('/:id', (req, res, next) => {
+    Message.findById(req.params.id)
+        .then(message => {
+            if (!message)
+                return res.status(500).json({
+                    title: 'No Message Found!',
+                    error: {
+                        message: 'Message Not Found!'
+                    }
+                });
+
+            message.content = req.body.content;
+            message.save()
+                .then(result => {
+                    res.status(200).json({
+                        message: 'Updated Message',
+                        obj: result
+                    });
+                });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                title: 'An Error Occurred',
+                error: err
+            });
+        });
+
+});
 
 module.exports = router;
