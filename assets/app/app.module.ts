8b80a03f18e4d19373dc9a0d4ccd02cfada42173
Ritwick Dey <ritwickdey@outlook.com>
2017-12-26 12:07:25 +0530

    refactored - MessageModule separated

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { MessageComponent } from './messages/message.component';
-import { MessageListComponent } from './messages/message-list';
-import { MessageInputComponent } from './messages/message-input.component';
-import { MessageService } from './messages/messages.service';
-import { MessagesComponent } from './messages/messages.component';
+
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
 import { routing } from './app-routing';
@@ -18,21 +14,18 @@ import { SignupComponent } from './auth/signup.component';
 import { AuthService } from './auth/auth.service';
 import { ErrorComponent } from './errors/error.component';
 import { ErrorService } from './errors/error.service';
+import { MessageModule } from './messages/message.module';
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule,
         ReactiveFormsModule,
         routing,
-        HttpClientModule
+        HttpClientModule,
+        MessageModule
     ],
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
         LogoutComponent,
@@ -41,7 +34,6 @@ import { ErrorService } from './errors/error.service';
         ErrorComponent
     ],
     providers: [
-        MessageService,
         AuthService,
         ErrorService
     ],
---
+++ assets/app/messages/message.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MessagesComponent } from './messages.component';
+import { MessageInputComponent } from './message-input.component';
+import { MessageListComponent } from './message-list';
+import { MessageComponent } from './message.component';
+import { MessageService } from './messages.service';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+    ],
+    exports: [],
+    declarations: [
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent,
+        MessagesComponent,
+    ],
+    providers: [
+        MessageService
+    ],
+})
+export class MessageModule { }
